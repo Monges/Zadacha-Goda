810ee8aa26ce361e07d858701676d685824794e6
Lesmo <Lesmo@KOMPUTER>
2024-08-15 13:01:26 +0300

    Добавьте файлы проекта.

---
+++ GodaIchisla.py
@@ -0,0 +1,51 @@
+# -*- coding: cp1251 -*-
+import datetime
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+#Без дататайма, pillow и нумпая работать не будет
+#Esli kirilica ne raboet cmenite codirovku v IDE na 1251
+
+ #1
+print('Пожалуйста, введите вашу дату рождения, пишите цифрами!')
+day = int(input('Введите день: '))
+month = int(input('Месяц: '))
+year = int(input('Год: '))
+TODAY = datetime.date.today()
+
+
+#2
+week_days=["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресень"]
+week_num=datetime.date(year,month,day).weekday()
+print(week_days[week_num])
+
+#3
+if year % 4 != 0:
+    print('Год не Високосный.')
+
+elif year % 100 == 0:
+    if year % 400 == 0:
+        print('Год високосный.')
+    else:
+        print('Год не високосный.')
+else:
+    print('Год високосный.')
+
+ #4
+print(f'Вам столько лет - {TODAY.year - year - ((TODAY.month, TODAY.day) < (month, day))} !')
+
+#5
+day = str(day) 
+month = str (month)
+year = str (year)
+text = (day + '  ' + month + '  ' + year)
+myfont = ImageFont.truetype("verdanab.ttf", 12)
+size = myfont.getbbox(text)[2:]
+img = Image.new("1",size,"black")
+draw = ImageDraw.Draw(img)
+draw.text((0, 0), text, "white", font=myfont)
+pixels = np.array(img, dtype=np.uint8)
+chars = np.array([' ','*'], dtype="U1")[pixels]
+strings = chars.view('U' + str(chars.shape[1])).flatten()
+print( "\n".join(strings))
+
+
